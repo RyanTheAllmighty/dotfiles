1a606f328a855c041adb1d61ab096f85b8349254
Ryan Dowling <ryan@ryandowling.me>
2018-04-06 04:15:14 +1000

    fix: actually fix sparse file not found issue

--- includes/linux/system.sh
+++ includes/linux/system.sh
@@ -2,4 +2,6 @@ function run_system_tweaks
 {
     # fix 'Sparse file not found' on boot
     sudo grub-editenv create
+    printf '# fix sparse file not found errors\nGRUB_SAVEDEFAULT=false\n' | sudo tee --append /etc/default/grub > /dev/null
+    sudo update-grub
 }
